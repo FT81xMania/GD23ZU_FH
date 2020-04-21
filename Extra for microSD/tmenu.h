50e07feae3ff2191a2f15ef475825c159a490f11
FT81x Manía <ft81xmania@gmail.com>
2020-04-21 12:14:28 +0200

    Add files via upload

---
+++ Extra for microSD/tmenu.h
@@ -0,0 +1,27 @@
+#define LOAD_ASSETS()  GD.safeload("tmenu.gd2");
+#define AV1_HANDLE 0
+#define AV1_WIDTH 510
+#define AV1_HEIGHT 480
+#define AV1_CELLS 1
+#define AV2_HANDLE 1
+#define AV2_WIDTH 290
+#define AV2_HEIGHT 480
+#define AV2_CELLS 1
+#define FT8XX_HANDLE 2
+#define FT8XX_WIDTH 200
+#define FT8XX_HEIGHT 130
+#define FT8XX_CELLS 1
+#define STM32_HANDLE 3
+#define STM32_WIDTH 275
+#define STM32_HEIGHT 100
+#define STM32_CELLS 1
+#define LLIMI_HANDLE 4
+#define LLIMI_WIDTH 90
+#define LLIMI_HEIGHT 90
+#define LLIMI_CELLS 1
+#define ASSETS_END 891200UL
+static const shape_t AV1_SHAPE = {0, 510, 480, 0};
+static const shape_t AV2_SHAPE = {1, 290, 480, 0};
+static const shape_t FT8XX_SHAPE = {2, 200, 130, 0};
+static const shape_t STM32_SHAPE = {3, 275, 100, 0};
+static const shape_t LLIMI_SHAPE = {4, 90, 90, 0};
