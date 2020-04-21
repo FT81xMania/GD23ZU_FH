720a68dc23463dd6719a92a2994e3f9360a5c366
FT81x Manía <ft81xmania@gmail.com>
2020-04-21 12:06:01 +0200

    Add files via upload

---
+++ Extra for microSD/logxx.h
@@ -0,0 +1,57 @@
+#define LOAD_ASSETS()  GD.safeload("logxx.gd2");
+#define LOG1_HANDLE 0
+#define LOG1_WIDTH 510
+#define LOG1_HEIGHT 480
+#define LOG1_CELLS 1
+#define LOG2_HANDLE 1
+#define LOG2_WIDTH 290
+#define LOG2_HEIGHT 480
+#define LOG2_CELLS 1
+#define LOGX_HANDLE 2
+#define LOGX_WIDTH 175
+#define LOGX_HEIGHT 114
+#define LOGX_CELLS 1
+#define STM32_HANDLE 3
+#define STM32_WIDTH 275
+#define STM32_HEIGHT 100
+#define STM32_CELLS 1
+#define LLIMI_HANDLE 4
+#define LLIMI_WIDTH 90
+#define LLIMI_HEIGHT 90
+#define LLIMI_CELLS 1
+#define RADIOS_HANDLE 5
+#define RADIOS_WIDTH 100
+#define RADIOS_HEIGHT 100
+#define RADIOS_CELLS 1
+#define RELOJ_HANDLE 6
+#define RELOJ_WIDTH 100
+#define RELOJ_HEIGHT 100
+#define RELOJ_CELLS 1
+#define SATELITE_HANDLE 7
+#define SATELITE_WIDTH 100
+#define SATELITE_HEIGHT 100
+#define SATELITE_CELLS 1
+#define TOOLS_HANDLE 8
+#define TOOLS_WIDTH 100
+#define TOOLS_HEIGHT 100
+#define TOOLS_CELLS 1
+#define NUBES_HANDLE 9
+#define NUBES_WIDTH 100
+#define NUBES_HEIGHT 100
+#define NUBES_CELLS 1
+#define MP3_HANDLE 10
+#define MP3_WIDTH 100
+#define MP3_HEIGHT 100
+#define MP3_CELLS 1
+#define ASSETS_END 999100UL
+static const shape_t LOG1_SHAPE = {0, 510, 480, 0};
+static const shape_t LOG2_SHAPE = {1, 290, 480, 0};
+static const shape_t LOGX_SHAPE = {2, 175, 114, 0};
+static const shape_t STM32_SHAPE = {3, 275, 100, 0};
+static const shape_t LLIMI_SHAPE = {4, 90, 90, 0};
+static const shape_t RADIOS_SHAPE = {5, 100, 100, 0};
+static const shape_t RELOJ_SHAPE = {6, 100, 100, 0};
+static const shape_t SATELITE_SHAPE = {7, 100, 100, 0};
+static const shape_t TOOLS_SHAPE = {8, 100, 100, 0};
+static const shape_t NUBES_SHAPE = {9, 100, 100, 0};
+static const shape_t MP3_SHAPE = {10, 100, 100, 0};
